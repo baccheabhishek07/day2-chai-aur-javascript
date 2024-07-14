c7edd673314e648c6a6e75757a1af5e31116a8a1
Baccheabhishek <baccheabhishek0707@gmail.com>
2024-07-14 12:43:48 +0530

    second commit

---
+++ app.js
@@ -0,0 +1,146 @@
+// Arithematic operations
+// task 1 (add two numbers)
+let num1=7;
+let num2=7;
+let result1=num1+num2;
+console.log("result1=",result1);
+
+// task 2 (substraction of two numbers)
+let num3=100;
+let num4=67;
+let result2=num3-num4;
+console.log("result2=",result2);
+
+//  task 3 (multiplication of two numbers)
+let num5=7;
+let num6=7;
+let result3=num5*num6;
+console.log("result3=",result3);
+
+//  task 4 (division of two numbers)
+let num7=56;
+let num8=4;
+let result4=num7/num8;
+console.log("result4=",result4);
+
+//   tsk 5 (find remainder of numbers)
+let num9=33;
+let num10=5;
+let result5=num9%num10;
+console.log("result5=",result5);
+
+// Assignment operators
+//   task 6 (+=)
+let num11=77;
+num11+=3;
+console.log("num11=",num11);
+
+//   task 7 (-=)
+let num12=77;
+num12-=2;
+console.log("num12=",num12);
+
+// comparison operator
+//  task 8 (><)
+let a = 18;
+if (a > 18) {
+    console.log("Your age is more than 18, so you can drive.");
+} else {
+    console.log("Your age is less than or equal to 18, so you can't drive.");
+}
+
+// task 9 (>= ,<=)
+let b = 18;
+if (a >=18) {
+    console.log("Your age is more than 18, so you can drive.");
+} else {
+    console.log("Your age is less than or equal to 18, so you can't drive.");
+}
+
+// task 10
+let c=10;
+let d="10";
+let result6=c==d;
+console.log("result6=",result6);
+
+// task 11
+let e=10;
+let f="10";
+let result7=c===d;
+console.log("result7=",result7);
+
+// logical operators
+// &&
+// task 12
+let isRaning=true;
+let haveUmbrella=true;
+if (isRaning && haveUmbrella){
+    console.log("you can go outside");
+}else{
+    console.log("you can't go outside");
+}
+
+//  task 13
+let isRaning1=false;
+let haveUmbrella1=true;
+if (isRaning1 && haveUmbrella1){
+    console.log("you can go outside");
+}else{
+    console.log("you can't go outside");
+}
+
+// ||
+// task 14
+let isRaning2=false;      
+let haveUmbrella2=true;
+if (isRaning2 || haveUmbrella2){
+    console.log("you can go outside");
+}else{
+    console.log("you can't go outside");
+}
+
+//  task 15
+let isRaning3=true;
+let haveUmbrella3=true;
+if (isRaning2 || haveUmbrella3){
+    console.log("you can go outside");
+}else{
+    console.log("you can't go outside");
+}
+
+//  task 16
+let isRaning4=false;
+let haveUmbrella4=false;
+if (isRaning2 || haveUmbrella4){
+    console.log("you can go outside");
+}else{
+    console.log("you can't go outside");
+}
+
+// !
+//  task 17
+let isRaning5=false;
+if (!isRaning5){
+    console.log("you can go outside");
+}else{
+    console.log("you can't go outside");
+}
+
+//  task 18
+let isRaning6=true;
+if (!isRaning6){
+    console.log("you can go outside");
+}else{
+    console.log("you can't go outside");
+}
+
+//  ternary operator
+// task 19
+let age=19;
+  age>18 ?console.log("adult"):console.log("not adult" )
+  
+
+
+
+
+
